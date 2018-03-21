c3792a8d0699a3047d524cdd32d30f30c784dac1
Benjamin <benjamin@simpulse.nl>
2018-03-21 16:38:41 +0100

    Added progress and input errors

--- xml_csv.py
+++ xml_csv.py
@@ -6,6 +6,7 @@ import xml.etree.ElementTree as ET
 import glob
 import json
 import argparse
+import sys
 
 # parse options given to script
 parser = argparse.ArgumentParser()
@@ -15,7 +16,7 @@ parser.add_argument("--namespaces", help="The namespaces to be used (.json)", me
 parser.add_argument("--output", help="The output file to be generated, defaults to source file name and location", metavar="")
 options = parser.parse_args()
 
-# get input values in terminal
+# save options to variables
 input_datafile = options.data
 input_structure = options.structure
 input_namespaces = options.namespaces or "examples/namespaces/namespaces.json"
@@ -24,29 +25,34 @@ input_outputfile = options.output or options.data + ".csv"
 # start timer
 timer = time.time()
 
-print("Working...")
+# output that we're doing something
+print("\nPreparing everything... (The script can be aborted by pressing ctrl+c)")
 
 # open and parse namespaces json
-namespacesfile = open(input_namespaces, "r")
+namespacesfile = open(input_namespaces, "r") if input_namespaces is not None and input_namespaces[-4:] == "json" else sys.exit("Error: Provided namespaces file is not a .json file.")
 namespaces = json.load(namespacesfile)
 
 # open and parse structure json
-structurefile = open(input_structure, "r")
+structurefile = open(input_structure, "r") if input_structure is not None and input_structure[-4:] == "json" else sys.exit("Error: Provided structure file is not a .json file.")
 structure = json.load(structurefile)
 
-# open file
-datafile = gzip.open(input_datafile)
-
-# read content
+# open and read data file
+if input_datafile[-2:] == "gz":
+    datafile = gzip.open(input_datafile)
+elif input_datafile[-3:] == "xml":
+    datafile = open(input_datafile, "r")
+else:
+    sys.exit("Error: Provided data source file is not a .gz or .xml file.")
 data = datafile.read()
 
-# parse data
+# parse xml data into an element tree
 tree = ET.fromstring(data)
+
+# initialize the row counter
+rows = 0
 
 # define the element reader
 def readElement(_structure, _tree, _vars):
-
-    # if (time.time() - timer) > 15: return
 
     # set default properties of the element
     _element = {
@@ -140,6 +146,12 @@ def readElement(_structure, _tree, _vars):
             global csv_writer
             csv_writer.writerow(row_vars)
 
+            # output to system
+            global rows
+            rows += 1
+            if rows % 150 == 0:
+                print("\rRows written: " + str(rows), end="")
+
 
 
         # element is not a row
@@ -222,12 +234,15 @@ def getColumnNames(_structure, _cols):
 fieldnames = getColumnNames(structure, [])
 
 # create a csv file
-csv_file = open(input_outputfile, 'w', newline='')
+csv_file = open(input_outputfile, 'w')
 csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames, extrasaction='ignore', delimiter=',')
 csv_writer.writeheader()
 
 # start with reading the structure
 readElement(structure, tree, {})
 
+# write the final number of rows written
+print("\rRows written: " + str(rows))
+
 # print duration of script execution
-print("Finished in " + str(time.time() - timer) + " seconds")
+print("Finished in " + str(round(time.time() - timer, 2)) + " seconds\n")
